6897c6ebe7c9f04c8a937c735ac189c4a15f3a2e
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2025-01-02 00:06:53 -0500

    Refactor main package structure and add health check endpoint
    - Refactored package from "server" to "main"
    - Added health check endpoint using Gin
    - Added flyer generation endpoint using flyer package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,26 @@
-package server
+package main
 
 import (
-	"log"
+	"github.com/Ibrahim-Haroon/ym-flyer-generator-server.git/internal/flyer"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"log"
+	"net/http"
 )
 
+func HealthCheck(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "healthy")
+}
 
 func main() {
 	err := godotenv.Load("local-config.env")
 	if err != nil {
 		log.Fatal("Error loading in enviornment file!")
-		return
 	}
 
-	// TODO
+	router := gin.Default()
+	router.GET("api/v1/health", HealthCheck)
+	router.POST("api/v1/flyer", flyer.GetBackground)
+
+	router.Run("localhost:8080")
 }
