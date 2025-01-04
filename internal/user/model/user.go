409d3a76da1cbeac5e7510829a0cb1002e943556
Ibrahim Haroon <ibrahim.haroon140@gmail.com>
2025-01-04 02:37:25 -0500

    Add user authentication model and update user model
    - Added LoginRequest and LoginResponse structs for user authentication
    - Added RegisterRequest, RegisterResponse, and UserRegisterationInfo structs for user registration
    - Updated User struct with additional fields and methods for user management
    - Implemented UpdateAPIKeysRequest and UpdateUserResponse structs for API key management
    - Created UserResponse struct for user data retrieval

--- internal/user/model/user.go
+++ internal/user/model/user.go
@@ -10,7 +10,33 @@ type User struct {
 	Username          string                           `json:"username"`
 	PasswordHash      string                           `json:"password_hash"`
 	Email             string                           `json:"email"`
+	CreatedAt         string                           `json:"created_at"`
+	UpdateAt          string                           `json:"updated_at"`
+	LastLogin         string                           `json:"last_login"`
+	ActiveStatus      bool                             `json:"active_status"`
 	IsAdmin           bool                             `json:"is_admin"`
 	TextModelApiKeys  map[textgen.ProviderType]string  `json:"text_model_api_keys"`
 	ImageModelApiKeys map[imagegen.ProviderType]string `json:"image_model_api_keys"`
+}
+
+type UpdateAPIKeysRequest struct {
+	TextProvider  string `json:"text_provider" binding:"required"`
+	TextAPIKey    string `json:"text_api_key" binding:"required"`
+	ImageProvider string `json:"image_provider" binding:"required"`
+	ImageAPIKey   string `json:"image_api_key" binding:"required"`
+}
+
+type UpdateUserResponse struct {
+	Message string `json:"message"`
+}
+
+type UserResponse struct {
+	ID           string `json:"id"`
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	CreatedAt    string `json:"created_at"`
+	UpdatedAt    string `json:"updated_at"`
+	LastLogin    string `json:"last_login"`
+	ActiveStatus bool   `json:"active_status"`
+	IsAdmin      bool   `json:"is_admin"`
 }
